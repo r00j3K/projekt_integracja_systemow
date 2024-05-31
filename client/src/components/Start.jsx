c3a692c2d679cc8d08ed60be37d4ae73aa89fa57
r00j3K <adrianr8@o2.pl>
2024-05-31 16:09:14 +0200

    start_page

--- client/src/components/Start.jsx
+++ client/src/components/Start.jsx
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 const Start = () => {
     return (
         <div class="d-flex justify-content-center align-items-center min-vh-100">
-            <div class="row">
+            <div>
                 <Link to="/login">
-                    <button class="btn btn-primary">Logowanie</button>
+                    <button class="btn btn-primary btn-lg mx-2">Logowanie</button>
                 </Link>
             </div>
-            <div class="row">
+            <div>
                 <Link to="/register">
-                    <button class="btn btn-outline-primary" >Rejestracja</button>
+                    <button class="btn btn-outline-primary btn-lg mx-2" >Rejestracja</button>
                 </Link>
             </div>
 
